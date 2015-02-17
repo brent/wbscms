63a203dda06e4a2edf028e0d027090fd27a03123
Brent Meyer <brent.e.meyer@gmail.com>
2015-02-16 22:27:53 -0500

    wip

--- app.rb
+++ app.rb
@@ -1,29 +1,43 @@
 require 'sinatra'
 
 get '/' do
-  all_paths = Dir.glob("posts/**/**")
-  all_post_paths = all_paths.delete_if { |path| !path.include? ".md" }
+  all_posts = Dir.glob("public/posts/*/*/*/*")
 
   @posts = []
-  all_post_paths.each do |path|
-    @posts.push(
-      { path: path.match(/posts\/(\d*\/\d*\/\d*\/.*)\..*/)[1],
-        title: path.match(/\/(\w*)\./)[1] }
-    )
+  all_posts.each do |path|
+    path['public/posts/'] = ''
+
+    if File.directory? path
+      @posts.push(
+        { path: path,
+          title: File.basename(path) }
+      )
+    else
+      @posts.push(
+        { path: path.split('.')[0],
+          title: File.basename(path, File.extname(path)) }
+      )
+    end
   end
 
+  @posts.each { |post| puts "XXX: #{post}" }
   erb :index, locals: { posts: @posts }
 end
 
 get '/:year/:month/:day/:title' do
-  require 'redcarpet'
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  if File.directory? "public/posts/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:title]}"
+    # send_file "public/posts/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:title]}/index.html"
+    redirect "posts/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:title]}/index.html"
+  else
+    require 'redcarpet'
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 
-  @post = ""
-  File.foreach("posts/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:title]}.md") do |line|
-    @post << line
+    @post = ""
+    File.foreach("public/posts/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:title]}.md") do |line|
+      @post << line
+    end
+    @parsed = markdown.render(@post)
+
+    erb :post, locals: { post: @parsed }
   end
-  @parsed = markdown.render(@post)
-
-  erb :post, locals: { post: @parsed }
 end
---
+++ public/posts/2015/01/14/test-2.md
@@ -0,0 +1,3 @@
+# Test 2
+
+This is *also* a test file.
